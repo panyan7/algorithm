39945f7efdaf20739abfb4cdaa5fb140225ecf36
elvis-pan <topanyan@sina.com>
2021-01-01 16:42:16 +0800

    Segment tree template

--- struct/segment_tree.cpp
+++ struct/segment_tree.cpp
@@ -3,35 +3,47 @@ using namespace std;
 
 const int MAXN = 10000;
 
-int n, t[4*MAXN], a[MAXN];
+int n;
+vector<int> x;
 
-void build(int v, int tl, int tr) {
-    if (tl == tr) {
-        t[v] = a[tl];
-    } else {
-        int m = tl + (tr-tl)/2;
-        build(v*2, tl, m);
-        build(v*2+1, m+1, tr);
-        t[v] = t[v*2] + t[v*2+1];
+template<typename T>
+struct segtree {
+    int n;
+    vector<T> t, a;
+private:
+    void build(int v, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int m = tl + (tr-tl)/2;
+            build(v*2, tl, m);
+            build(v*2+1, m+1, tr);
+            t[v] = t[v*2] + t[v*2+1];
+        }
     }
-}
-
-int sum(int v, int tl, int tr, int l, int r) {
-    if (l > r) return 0;
-    if (l == tl && r == tr) return t[v];
-    int m = tl + (tr-tl)/2;
-    return sum(v*2, tl, m, l, min(r, m)) 
-         + sum(v*2+1, m+1, tr, max(l, m+1), r);
-}
-
-void update(int v, int tl, int tr, int pos, int val) {
-    if (tl == tr) {
-        t[v] = val;
-    } else {
-        int m = tl + (tr-tl)/2;
-        if (pos <= m) update(v*2, tl, m, pos, val);
-        else update(v*2+1, m+1, tr, pos, val);
-        t[v] = t[v*2] + t[v*2+1];
+public:
+    segtree (vector<T>& b) {
+        a = b;
+        int n = a.size();
+        t = vector<T> (4*n);
+        build(1, 0, n-1);
     }
-}
+    int sum(int v, int tl, int tr, int l, int r) {
+        if (l > r) return 0;
+        if (l == tl && r == tr) return t[v];
+        int m = tl + (tr-tl)/2;
+        return sum(v*2, tl, m, l, min(r, m)) 
+             + sum(v*2+1, m+1, tr, max(l, m+1), r);
+    }
+    void update(int v, int tl, int tr, int pos, int val) {
+        if (tl == tr) {
+            t[v] = val;
+        } else {
+            int m = tl + (tr-tl)/2;
+            if (pos <= m) update(v*2, tl, m, pos, val);
+            else update(v*2+1, m+1, tr, pos, val);
+            t[v] = t[v*2] + t[v*2+1];
+        }
+    }
+};
 

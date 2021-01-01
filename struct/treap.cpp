292515ebd9cb9341e97b25af91bb1d803eba83ab
elvis-pan <topanyan@sina.com>
2021-01-01 09:50:37 +0800

    Fix friend function in treap

--- struct/treap.cpp
+++ struct/treap.cpp
@@ -6,13 +6,10 @@ struct node {
     T key;
     int prior;
     node<T> *l, *r;
+
     node () {}
-    node (T k, int p) {
-        key = k;
-        prior = p;
-        l = nullptr;
-        r = nullptr;
-    }
+    node (T k, int p) { key = k; prior = p; l = nullptr; r = nullptr; }
+
 private:
     typedef node * node_t;
 
@@ -23,15 +20,20 @@ public:
     friend bool operator < (const node& a, const node& b) {
         return a.key < b.key;
     }
-
-    friend void split(node_t t, T key, node_t low, node_t hi) {
+    friend bool operator == (const node& a, const node& b) {
+        return a.key == b.key;
+    }
+    friend ostream& operator << (ostream& o, const node& a) {
+        o << a.key; return o;
+    }
+    static void split(node_t t, T key, node_t low, node_t hi) {
         if (t == nullptr) low = hi = nullptr;
         else if (key < t->key)
             split(t->l, key, low, t->l), hi = t;
         else
             split(t->r, key, t->r, hi), low = t;
     }
-    friend void insert(node_t& t, node_t it) {
+    static void insert(node_t& t, node_t it) {
         if (t == nullptr)
             t = it;
         else if (it->prior > t->prior)
@@ -39,7 +41,7 @@ public:
         else
             insert(it->key < t->key ? t->l : t->r, it);
     }
-    friend void merge(node_t& t, node_t l, node_t r) {
+    static void merge(node_t& t, node_t l, node_t r) {
         if (!l || !r)
             t = l ? l : r;
         else if (l->prior > r->prior)
@@ -47,7 +49,7 @@ public:
         else
             merge(r->l, l, r->l), t = r;
     }
-    friend void erase(node_t& t, T key) {
+    static void erase(node_t& t, T key) {
         if (t->key == key) {
             node_t th = t;
             merge(t, t->l, t->r);
@@ -56,7 +58,7 @@ public:
             erase(key < t->key ? t->l : t->r, key);
         }
     }
-    friend node_t unite(node_t l, node_t r) {
+    static node_t unite(node_t l, node_t r) {
         if (!l || !r) return l ? l : r;
         if (l->prior < r->prior) swap(l, r);
         node_t lt, rt;
@@ -70,14 +72,32 @@ public:
 template <typename T>
 struct treap {
     node<T> *root;
+
     treap () { root = nullptr; }
-    treap (T d, int p) { *root = node<T> (d, p); }
-    treap (node<T>* n) { root = n; }
+    treap (T d, int p) { root = new node<T> (d, p); }
+    treap (node<T> *n) { root = n; }
 public:
+    // Overload operator
+    friend bool operator < (const treap& a, const treap& b) {
+        return *a.root < *b.root;
+    }
+    friend bool operator > (const treap& a, const treap& b) {
+        return *a.root > *b.root;
+    }
+    friend bool operator == (const treap& a, const treap& b) {
+        return *a.root == *b.root;
+    }
+    friend ostream& operator << (ostream& o, const treap& a) {
+        o << *a.root; return o;
+    }
+
+    // Access variables
     T key() { assert(root != nullptr); return root->key; }
     int priority () { assert(root != nullptr); return root->prior; }
     treap l() const { assert(root != nullptr); return treap(root->l); }
     treap r() const { assert(root != nullptr); return treap(root->r); }
+
+    // Functions
     treap split(int key, treap& low, treap& hi) {
         node<T>::split(root, key, low.root, hi.root);
         return *this;
@@ -90,25 +110,25 @@ public:
         node<T>::merge(root, l.root, r.root);
         return *this;
     }
-    treap erase(int key) {
+    treap erase(T key) {
         node<T>::erase(root, key);
         return *this;
     }
-    friend treap unite(treap& l, treap& r) {
-        node<T> *res = unite(l.root, r.root);
+    static treap unite(treap& l, treap& r) {
+        node<T> *res = node<T>::unite(l.root, r.root);
         return treap(res);
     }
 };
 
 int main() {
-    node<int>* n1 = new node<int> (1, 1);
-    node<int>* n2 = new node<int> (2, 0);
-    treap<int> t1 (n1), t2 (n2);
+    treap<int> t1 (1,1), t2 (2,0), t3 (3,2);
     cout << t1.key() << " " << t2.key() << endl;
     t1.insert(t2);
-    // insert(n1, n2);
-    // cout << n1->key << " " << n1->r->key << endl;
-    // cout << t2.l().key() << " " << t2.l().priority() << endl;
-    // cout << t2.r().key() << " " << t2.r().priority() << endl;
+    t1.erase(1);
+    t1.merge(t2, t3);
+    treap<int> t4 = treap<int>::unite(t2, t3);
+    cout << t1.key() << endl;
+    cout << t1.l().key() << endl;
+    cout << t4 << t4.l() << endl;
     return 0;
 }

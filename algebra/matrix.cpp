3ed748679fc7f99a6c9f8f99c9e60f613cc00d7b
elvis-pan <topanyan@sina.com>
2021-01-11 15:58:06 +0800

    Update union find

--- algebra/matrix.cpp
+++ algebra/matrix.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define ld long double
-#define all(c) (c).begin(), (c).end()
-#define F_OR1(n) for (int i = 0; i < n; ++i)
-#define F_OR2(i, n) for (int i = 0; i < n; ++i)
-#define F_OR3(i, k, n) for (int i = k; i < n; ++i)
-#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
-#define F_OR(...) GET_MACRO(__VA_ARGS__, F_OR3, F_OR2, F_OR1)
-#define FOR(...) F_OR(__VA_ARGS__) (__VA_ARGS__)
-#define EACH(x, a) for (auto& x : a)
-const int MOD = 1e9+7;
 
 template <typename T> 
 struct matrix {
--- graph/union_find.cpp
+++ graph/union_find.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> parents;
+vector<int> parent;
+vector<int> sz;
 
-int union_find() {
-    return -1;
+void make_set(int v) {
+    parent[v] = v;
+}
+
+int find_set(int v) {
+    if (parent[v] == v) return v;
+    return find_set(parent[v]);
+}
+
+bool union_sets(int v, int u) {
+    v = find_set(v), u = find_set(u);
+    if (v == u) return false;
+    parent[u] = v;
+    sz[v] += sz[u];
+    return true;
 }

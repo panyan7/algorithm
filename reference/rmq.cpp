68691fa634f71a3f1661c2d4a52029f2633b04bf
elvis-pan <topanyan@sina.com>
2021-01-15 21:26:21 +0800

    Add reference solution to RMQ

--- reference/rmq.cpp
+++ reference/rmq.cpp
@@ -1,3 +1,8 @@
+/*
+    Reference Solution using Range Minimun Query
+    Codeforces 1473 D "Program"
+    https://codeforces.com/contest/1473/problem/D
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -38,7 +43,26 @@ public:
 int t, n, m;
 
 void solve() {
-
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    vector<int> arr (n+1, 0);
+    for (int i = 1; i <= n; ++i)
+        arr[i] = arr[i-1] + (s[i-1] == '+' ? 1 : -1);
+    RMQ mn (arr), mx (arr, true);
+    int l, r;
+    for (int i = 0; i < m; ++i) {
+        cin >> l >> r;
+        int mn1 = mn.query(0,l-1);
+        int mx1 = mx.query(0,l-1);
+        if (r == n) {
+            cout << (mx1-mn1+1) << "\n";
+            continue;
+        }
+        int mn2 = mn.query(r+1,n)-arr[r]+arr[l-1];
+        int mx2 = mx.query(r+1,n)-arr[r]+arr[l-1];
+        cout << (max(mx2,mx1)-min(mn2,mn1)+1) << "\n";
+    }
 }
 
 int main() {

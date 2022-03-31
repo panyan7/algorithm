a03e56f77fca9e0020b1e614c140e313f927b1e9
topanyan <topanyan@sina.com>
2022-03-30 21:31:48 -0400

    Add KMP

--- string/kmp.cpp
+++ string/kmp.cpp
@@ -1,38 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long long,long long>
 
-/**
- * Knuth-Morris-Pratt algorithm for finding the occurrence of a certain word 
- * from some text.
- */
-int kmp_search(string s, string w) {
-    int m = s.size(), n = w.size();
-    if (n == 0) return 0;
+int tt = 1, n, m, k;
 
-    // compute table using two pointers
-    int t[n];
-    t[0] = -1; // must restart all
-    for (int p = 1, q = 0; p < n; ++p, ++q) {
-        if (w[p] == w[q]) t[p] = t[q];
-        else {
-            t[p] = q;
-            q = t[q];
-            while (q >= 0 && w[p] != w[q]) q = t[q];
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+void compute_automaton(string s, vector<vector<int>>& aut) {
+    s += '#';
+    int n = s.size();
+    vector<int> pi = prefix_function(s);
+    aut.assign(n, vector<int>(26));
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
         }
     }
+}
 
-    // search
-    for (int i = 0, j = 0; i < m; ) {
-        if (w[j] == s[i]) {
-            ++i; ++j;
-            if (j == n) return i-j; // found
-        } else {
-            assert(j < n);
-            j = t[j];
-            if (j < 0) { // start again
-                ++i; ++j;
-            }
-        }
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> tt;
+    while (tt--) {
+        solve();
     }
-    return -1;
+    return 0;
 }

08feecaf9dea99db1c74ddb21a2e22b885ce00d9
topanyan <ypan2@andrew.cmu.edu>
2022-05-20 12:24:52 -0400

    Combinatorics

---
+++ combinatorics/combinatorics.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<int64_t,int64_t>
+
+template <int MOD>
+struct ModNum {
+    int v;
+    ModNum() : v(0) {}
+    ModNum(int64_t v_) : v(int((v_ + MOD) % MOD)) {}
+    explicit operator int() const { return v; }
+    friend ostream& operator<<(ostream& os, const ModNum& m) {
+        return os << m.v;
+    }
+    friend istream& operator>>(istream& is, ModNum& m) {
+        return is >> m.v;
+    }
+    friend bool operator==(const ModNum& a, const ModNum& b) {
+        return a.v == b.v;
+    }
+    friend bool operator!=(const ModNum& a, const ModNum& b) {
+        return !(a == b);
+    }
+    ModNum& operator++() {
+        ++v;
+        if (v == MOD) v = 0;
+        return *this;
+    }
+    ModNum operator++(int) {
+        ModNum r = *this;
+        ++*this;
+        return r;
+    }
+    ModNum& operator--() {
+        --v;
+        if (v == MOD) v = 0;
+        return *this;
+    }
+    ModNum operator--(int) {
+        ModNum r = *this;
+        --*this;
+        return r;
+    }
+    ModNum neg() const { return ModNum(v == 0 ? 0 : MOD - v); }
+private:
+    static int minv(int a, int m) {
+        if (a <= 1) return a;
+        return m - int(int64_t(minv(m % a, a)) * m / a);
+    }
+public:
+    ModNum inv() const { assert(v); return ModNum(minv(v, MOD)); }
+    ModNum& operator+=(const ModNum& o) {
+        v -= MOD - o.v;
+        v = (v < 0 ? v + MOD : v);
+        return *this;
+    }
+    ModNum& operator-=(const ModNum& o) {
+        v -= o.v;
+        v = (v < 0 ? v + MOD : v);
+        return *this;
+    }
+    ModNum& operator*=(const ModNum& o) {
+        v = int(int64_t(v) * int64_t(o.v) % MOD);
+        return *this;
+    }
+    ModNum& operator/=(const ModNum& o) {
+        return *this *= o.inv();
+    }
+    friend ModNum operator+(const ModNum& a, const ModNum& b) {
+        return ModNum(a) += b;
+    }
+    friend ModNum operator-(const ModNum& a, const ModNum& b) {
+        return ModNum(a) -= b;
+    }
+    friend ModNum operator*(const ModNum& a, const ModNum& b) {
+        return ModNum(a) *= b;
+    }
+    friend ModNum operator/(const ModNum& a, const ModNum& b) {
+        return ModNum(a) /= b;
+    }
+    friend ModNum pow(const ModNum& a, int e) {
+        ModNum res = 1;
+        ModNum b(a);
+        while (e) {
+            if (e % 2) res *= b;
+            e /= 2;
+            b *= b;
+        }
+        return res;
+    }
+};
+
+const int MOD = 1e9+7;
+// const int MOD = 998244353;
+using num = ModNum<MOD>;
+int tt = 1, n, m;
+const int MAXN = 1e6;
+num fact[MAXN+10], ifact[MAXN+10];
+
+void init() {
+    fact[0] = 1;
+    for (int i = 1; i <= MAXN; i++)
+        fact[i] = fact[i-1] * i;
+    for (int i = 0; i <= MAXN; i++)
+        ifact[i] = fact[i].inv();
+}
+
+num A(int a, int b) {
+    if (b < 0 || b > a) return 0;
+    return fact[a] * ifact[a-b];
+}
+
+num C(int a, int b) {
+    if (b < 0 || b > a) return 0;
+    return fact[a] * ifact[a-b] * ifact[b];
+}
+
+void solve() {
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    init();
+    cin >> tt;
+    while (tt--)
+        solve();
+    return 0;
+}

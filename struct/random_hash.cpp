eeae225760801b6d5d7247994a2e5ab9c831e33c
topanyan <ypan2@andrew.cmu.edu>
2022-05-31 17:57:42 -0400

    Add split mix hash function

--- struct/random_hash.cpp
+++ struct/random_hash.cpp
@@ -11,8 +11,21 @@ struct RandomHash {
         return x ^ (x >> 16);
     }
 };
-typedef unordered_set<ll, RandomHash> safe_set;
-typedef unordered_map<ll, int, RandomHash> safe_map;
+struct SplitMixHash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+typedef unordered_set<ll, SplitMixHash> safe_set;
+typedef unordered_map<ll, int, SplitMixHash> safe_map;
 
 int tt = 1, n, m, k;
 

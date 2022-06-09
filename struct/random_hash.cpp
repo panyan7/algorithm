20da56dd1c4e041daa5d28b1616b91637abd7840
topanyan <ypan2@andrew.cmu.edu>
2022-06-08 23:01:00 -0400

    Add hash function for string

--- struct/random_hash.cpp
+++ struct/random_hash.cpp
@@ -24,8 +24,34 @@ struct SplitMixHash {
         return splitmix64(x + FIXED_RANDOM);
     }
 };
-typedef unordered_set<ll, SplitMixHash> safe_set;
-typedef unordered_map<ll, int, SplitMixHash> safe_map;
+struct StringHash {
+    static uint64_t hash_str(string const& s) {
+        const uint64_t p = 31;
+        const uint64_t m = 1e9 + 7;
+        uint64_t h = 0;
+        uint64_t p_pow = 1;
+        for (char c : s) {
+            h = (h + (c - 'a' + 1) * p_pow) % m;
+            p_pow = (p_pow * p) % m;
+        }
+        return h;
+    }
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(const string& s) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(hash_str(s) + FIXED_RANDOM);
+    }
+};
+
+// Example of defining sets
+// typedef unordered_set<ll, SplitMixHash> safe_set;
+// typedef unordered_map<ll, int, SplitMixHash> safe_map;
 
 int tt = 1, n, m, k;
 

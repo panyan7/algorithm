69c2c9a5b7e0f8bbc9539b9da1651bcbfee7e427
topanyan <ypan2@andrew.cmu.edu>
2023-11-05 01:35:59 -0400

    Update printing stuff

--- .print/print.py
+++ .print/print.py
@@ -1,10 +1,11 @@
 import os
+import argparse
 
-def read_file(filename):
+def read_file(filename, write_filename="print.md"):
     with open(filename, 'r') as f:
         text = f.read()
-    with open("print.md", 'a') as f:
-        f.write("\n\\pagebreak\n")
+    with open(write_filename, 'a') as f:
+        # f.write("\n\\pagebreak\n")
         f.write("\n# " + filename + "\n")
         f.write("```cpp\n")
         f.write(text)
@@ -12,6 +13,7 @@ def read_file(filename):
 
 with open("README.md", 'r') as f:
     readme = f.read()
+
 with open("print.md", 'w') as f:
     f.write(readme)
     f.write("\n")
@@ -23,3 +25,9 @@ for folder in os.listdir():
                 read_file(filename)
                 print(filename)
 
+with open(".print/print_list.txt", 'r') as f:
+    print_list = f.read()
+for filename in print_list.split('\n'):
+    if filename != '':
+        read_file(filename + '.cpp', write_filename="print_large.md")
+        print(filename)

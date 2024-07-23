2d2e991ef4723cdeaab2726672e73f24e12b2f22
panyan7 <ypan2@andrew.cmu.edu>
2024-07-23 13:33:11 -0400

    Update some functions to lambdas

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -6,27 +6,26 @@ using namespace std;
 
 const int INF = 1e9;
 int n, m;
-vector<array<ll,3>> edges;
-vector<int> d;
-
-void bellman_ford(int s) {
-    d.assign(n+1, INF);
-    d[s] = 0;
-    for (int k = 1; k <= n-1; ++k) {
-        bool upd = false;
-        for (auto e : edges) {
-            if (d[e[0]] < INF) {
-                if (d[e[1]] > d[e[0]] + e[2]) {
-                    d[e[1]] = d[e[0]] + e[2];
-                    upd = true;
-                }
-            }
-        }
-        if (!upd) return;
-    }
-}
 
 void solve() {
+    vector<array<ll,3>> edges;
+    vector<int> d;
+    auto bellman_ford = [&](int s) -> void {
+        d.assign(n+1, INF);
+        d[s] = 0;
+        for (int k = 1; k <= n-1; ++k) {
+            bool upd = false;
+            for (auto e : edges) {
+                if (d[e[0]] < INF) {
+                    if (d[e[1]] > d[e[0]] + e[2]) {
+                        d[e[1]] = d[e[0]] + e[2];
+                        upd = true;
+                    }
+                }
+            }
+            if (!upd) return;
+        }
+    };
 }
  
 int main() {
